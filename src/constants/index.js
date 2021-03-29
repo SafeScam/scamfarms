aa374b1ff77ac682832fbd460f50460f5bbdba4d
duncan4123 <54425677+duncan4123@users.noreply.github.com>
2021-03-29 15:15:58 +1100

    update subgraph to ac one

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,7 @@
 // GraphQL URIs
 export const HONEYSWAP_GQL_URI = 'https://api.thegraph.com/subgraphs/name/1hive/uniswap-v2'
-export const HONEYCOMB_GQL_URI = 'https://api.thegraph.com/subgraphs/name/1hive/honeycomb'
+export const HONEYCOMB_GQL_URI = 'https://api.thegraph.com/subgraphs/name/adept-camp/honeycomb-subgraph'
+
 
 // EVM constants
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
